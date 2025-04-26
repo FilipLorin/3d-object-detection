86eed50745c1bfd9dc176cdbd299c4048e873d1c
Mr-Moose-dev <filip.lorin@gmail.com>
2025-04-26 20:17:01 +0200

    minimalna detekcja

--- yolo/detect.py
+++ yolo/detect.py
@@ -2,8 +2,12 @@
 # OUTPUT: [box, category] list
 
 # Requirements: ultralytics, pip 
-
+import cv2
 from ultralytics import YOLO
 
 # Download and load in pretrained COCO detection model
 model = YOLO("yolo11n.pt")
+
+im2 = cv2.imread("bus.jpg")
+results = model.predict(source=im2, save=True, save_txt=True)  # save predictions as labels
+
